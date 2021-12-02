ace72f1c18748a2fa1606e2dfb36fef3bdb2c569
Osama <osama3akre@gmail.com>
2021-12-02 22:53:33 +0300

    first commit

--- src/gmail.php
+++ src/gmail.php
@@ -16,7 +16,7 @@ function getClient()
     $client = new Google_Client();
     $client->setApplicationName('Gmail API PHP Quickstart');
     $client->setScopes(Google_Service_Gmail::GMAIL_MODIFY);
-    $client->setAuthConfig('./../conf.json');
+    $client->setAuthConfig(__DIR__ .'/../conf.json');
     $client->setAccessType('offline');
     $client->setPrompt('select_account consent');
 
@@ -24,7 +24,7 @@ function getClient()
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-    $tokenPath = './../token.json';
+    $tokenPath = __DIR__ .'/../token.json';
     if (file_exists($tokenPath)) {
         $accessToken = json_decode(file_get_contents($tokenPath), true);
         $client->setAccessToken($accessToken);
